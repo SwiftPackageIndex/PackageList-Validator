14463b0c463ee6e716d1bcf31db23059ff0bea81
Sven A. Schmidt <sas@finestructure.co>
2020-10-15 11:37:38 +0200

    Add Version subcommand

--- Sources/ValidatorCore/Commands/Validator.swift
+++ Sources/ValidatorCore/Commands/Validator.swift
@@ -4,8 +4,24 @@ import ArgumentParser
 public struct Validator: ParsableCommand {
     public static var configuration = CommandConfiguration(
         abstract: "SPI Validator",
-        subcommands: [CheckDependencies.self, CheckRedirects.self]
+        subcommands: [CheckDependencies.self, CheckRedirects.self, Version.self],
+        defaultSubcommand: Version.self
     )
+
     public mutating func run() throws {}
+
     public init() {}
+}
+
+
+extension Validator {
+    struct Version: ParsableCommand {
+        @Flag(name: [.customLong("version"), .customShort("v")],
+              help: "Show version")
+        var showVersion: Bool = false
+
+        mutating func run() throws {
+            print(AppVersion)
+        }
+    }
 }
---
+++ Sources/ValidatorCore/Version.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sven A. Schmidt on 15/10/2020.
+//
+
+import Foundation
